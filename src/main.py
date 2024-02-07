aa029beacbcb180d6c176852716439d953b1bbba
AltCh <christoph.alt@rhimagnesita.com>
2024-02-07 19:44:52 +0100

    feat(todo app): added delete function

--- src/main.py
+++ src/main.py
@@ -7,27 +7,32 @@ app = FastAPI()
 todos: list[Todo] = []
 
 
-# Get all todos
 @app.get("/todos")
-async def get_todos() -> dict[str, list[Todo]]:
+async def get_all_todos() -> dict[str, list[Todo]]:
     return {"todos": todos}
 
 
-# Get a single todo
 @app.get("/todos/{todo_id}")
 async def get_single_todo(todo_id: int) -> dict[str, Todo | str]:
     for todo in todos:
         if todo.id == todo_id:
             return {"todo": todo}
-        return {"message": "Todo not found."}
+    return {"message": "Todo not found."}
 
 
-
-# Create a new todo
 @app.post("/todos")
-async def create_todos(todo: Todo) -> dict[str, str]:
+async def create_single_todo(todo: Todo) -> dict[str, str]:
     todos.append(todo)
     return {"message": "Todo has been added successfully."}
 
 
-# Update a todo
+# Update a to_do
+
+# Delete a to_do
+@app.delete("/todos/{todo_id}")
+async def delete_single_todo(todo_id: int) -> dict[str, str]:
+    for todo in todos:
+        if todo.id == todo_id:
+            todos.remove(todo)
+            return {"message": "Todo has been removed successfully."}
+    return {"message": "Todo not found."}

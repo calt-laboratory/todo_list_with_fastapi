fbcd98cad421019e31811155deceaf89ac0e0b88
AltCh <christoph.alt@rhimagnesita.com>
2024-02-07 22:46:25 +0100

    feat(database): added database setup

---
+++ src/database.py
@@ -0,0 +1,19 @@
+import databases
+import sqlalchemy
+
+DATABASE_URL = "sqlite:///./test.db"
+database = databases.Database(DATABASE_URL)
+metadata = sqlalchemy.MetaData()
+
+todos = sqlalchemy.Table(
+    "todos",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("item", sqlalchemy.String),
+    sqlalchemy.Column("completed", sqlalchemy.Boolean),
+)
+
+engine = sqlalchemy.create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)
+metadata.create_all(engine)
